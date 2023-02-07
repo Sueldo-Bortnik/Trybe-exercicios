864cbf996bf05240dff8855558def9105023b711
˜sueldo <sueldo_bortnik@hotmail.com>
2023-02-07 15:51:06 -0300

    att

--- Projetos/sd-30-b-project-pixels-art/script.js
+++ Projetos/sd-30-b-project-pixels-art/script.js
@@ -9,35 +9,46 @@ const button = document.getElementById('button-random-color');
 function randColor(color) {
     color.style.backgroundColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
     return color;
-}
+  }
 
+
+  window.addEventListener('load', () => {
+    for (let i = 0; i < cArray.length; i += 1) {
+        const color = localStorage.getItem(`colorPalette${i}`);
+        if (color) {
+           cArray[i].style.backgroundColor = color;
+        }
+    }
+  });
 button.addEventListener('click', () => { 
     for(let i = 0; i < cArray.length; i +=1) {
         randColor(cArray[i]);
       console.log(`rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`)
+      localStorage.setItem(`colorPalette${i}`, cArray); // salvando as cores.
+      
     }
 });
 
-// requisito 5: 
+// requisito recuperar 5: 
 
 
 // requisito 6 e 7:
 
-const board = (num) => {
-    const pBoard = document.getElementById('pixel-board');
+function board(num) {
+  const pBoard = document.getElementById('pixel-board');
 
-    for (let i = 0; i < num; i += 1) {
-      const pLine = document.createElement('div');
-      pLine.classList.add('line');
-      pBoard.appendChild(pLine);
+  for (let i3 = 0; i3 < num; i3 += 1) {
+    const pLine = document.createElement('div');
+    pLine.classList.add('line');
+    pBoard.appendChild(pLine);
 
-      for (let i2 = 0; i2 < num; i2 += 1) {
-        const pItem = document.createElement('div');
-        pItem.classList.add('pixel');
-        pLine.appendChild(pItem);
-      }
+    for (let i2 = 0; i2 < num; i2 += 1) {
+      const pItem = document.createElement('div');
+      pItem.classList.add('pixel');
+      pLine.appendChild(pItem);
     }
-  };
+  }
+}
   board(5);
 
   // resuitiso 8:
